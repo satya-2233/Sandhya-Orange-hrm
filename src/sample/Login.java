b3d20ee4c8fb176c60c82fbd52bd5e02f56d5dd8
Sandhya <Sandhya@LAPTOP-D71N0MIA>
2018-03-07 08:42:31 +0530

    sample

---
+++ src/sample/Login.java
@@ -0,0 +1,17 @@
+package sample;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Login 
+{
+
+	
+	public static void launch(){
+		
+	//Launch the application
+	WebDriver driver = new FirefoxDriver();
+	
+	}
+	
+}
